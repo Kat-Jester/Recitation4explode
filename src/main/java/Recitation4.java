924ab45c415c963562805f562f9ab8e8e25bf39c
Kat-Jester <hortonhandlesit@gmail.com>
2025-02-21 19:02:26 -0500

    Recitaiton 4 complete

--- src/main/java/Recitation4.java
+++ src/main/java/Recitation4.java
@@ -10,20 +10,40 @@ public class Recitation4 {
      *
      * @param word a string that is to be exploded
      * @return a string generated from word by duplicating
-     *        each letter in the string based on the character's position. Digits
-     *        are duplicated by the value of their digit. Any other character
-     *        considered non-alphanumeric will result in all characters being
-     *        appended to the result, up to but not including the non-alphanumeric
-     *        character
+     * each letter in the string based on the character's position. Digits
+     * are duplicated by the value of their digit. Any other character
+     * considered non-alphanumeric will result in all characters being
+     * appended to the result, up to but not including the non-alphanumeric
+     * character
      */
     public static String explode(String word){
         StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length();i++){
+            char ch = word.charAt(i);
 
-        // TODO: replace this line with your code
+            if (Character.isLetter(ch)){
+                for(int m = 0; m <= i ; m++){
+                    sb.append(ch);
+                }
+            }else if(Character.isDigit(ch)){
+                int num = Character.getNumericValue(ch);
+                for (int s = 0; s < num; s++ )
+                    sb.append(ch);
+            }else {
+                sb.append(word.substring(0,i));
+            }
+        }
 
         return sb.toString();
     }
 }
 /*
-TODO: Replace this line with your algorithm
+Step 1: Get word length
+Step 2: Find position for letter in the string
+Step 3: If letter is non-alphanumeric add full string to empty string
+Step 4:If is digit append string for amount digit
+Step 5: If is letter append empty string by current letter position
+Step 6: go back to step 2
+
+
 */
